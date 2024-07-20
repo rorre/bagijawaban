9a72ce44be8e28e2979d92796339a6a43bdf3b05
Rendy Arya Kemal <renrror@gmail.com>
2024-07-20 10:30:02 +0700

    feat: add script to upload from dir

---
+++ scripts/insert_from_dir.py
@@ -0,0 +1,27 @@
+import asyncio
+from pathlib import Path
+import sys
+from typing import cast
+
+import frontmatter
+from ulid import ULID
+from src.db import get_db_conn
+
+
+async def main():
+    if len(sys.argv) != 3:
+        return print(f"{sys.argv[0]} <dir> <course_id>")
+
+    db = await get_db_conn()
+
+    for fpath in Path(sys.argv[1]).glob("*.md"):
+        with open(fpath, encoding="utf8", errors="ignore") as f:
+            post = frontmatter.load(f)
+
+        await db.execute(
+            "INSERT INTO assignments (id, name, description, course_id) VALUES (?, ?, ?, ?)",
+            [str(ULID()), cast(str, post.metadata["title"]), post.content, sys.argv[2]],
+        )
+
+
+asyncio.run(main())
